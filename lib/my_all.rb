cff0495d273cbb4add89354b67a5543b8485248b
Jed Peek <jedpeek@yahoo.com>
2018-06-04 22:52:33 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -3,9 +3,15 @@ require 'pry'
 def my_all?(collection)
 i = 0
 block_return = []
-while i < collection.size
-  block_return << yield(collection[i])
-  i += 1
+  while i < collection.size
+    block_return << yield(collection[i])
+    i += 1
+  end
+  if block_return.include?(false)
+    false
+  else
+    true
+  end
 end
-block_return
-end
+
+my_all?(collection){|x| x.upcase}
